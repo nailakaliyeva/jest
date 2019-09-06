f32a927c0d42902780cf0c8d119c6bf6b798fb60
Naila <nailakaliyeva@gmail.com>
2019-09-06 17:17:40 +0000

    sometimes I might not have access to a particular function. Thats when you use a mock function. Usially you need to use in in some type of integrity testings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-export const add = (x, y)=>{
-    return x+y
-}
+// export const add = (x, y)=>{
+//     return x+y
+// }
 export const total = (shipping, subTotal)=>{
     return "$"+ add(shipping, subTotal)
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,16 @@
-import {add, total} from "./App";
+import { total} from "./App";
 // test("Fake Test", ()=>{
 //     expect(true).toBeTruthy();
 // })
 
+const add = jest.fn(()=>3)
 test("add", ()=>{
     const value = add(1,2)
-
     expect(value).toBe(3)
+    expect(add).toHaveBeenCalledTimes(1)
+    expect(add).toHaveBeenCalledWith(1,2)
+
 })
-test("total", ()=>{
-    expect (total(5,20)).toBe("$25")
-})
+// test("total", ()=>{
+//     expect (total(5,20)).toBe("$25")
+// })
