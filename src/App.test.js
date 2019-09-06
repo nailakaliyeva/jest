280b93ccfb6b20c65e99714387af1178669185d5
Naila <nailakaliyeva@gmail.com>
2019-09-06 16:11:51 +0000

    add function

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,10 @@
-test("Fake Test", ()=>{
-    expect(true).toBeTruthy();
+import {add} from "./App";
+// test("Fake Test", ()=>{
+//     expect(true).toBeTruthy();
+// })
+
+test("add", ()=>{
+    const value = add(1,2)
+
+    expect(value).toBe(3)
 })
